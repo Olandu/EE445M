826a1cf14250d60528dfd6a7d1adc2502092163a
Olandu <writechioma@utexas.edu>
2018-02-02 22:13:37 -0600

    final

--- Lab 01 Graphics, LCD, Timer, and Interrupts/src/main.c
+++ Lab 01 Graphics, LCD, Timer, and Interrupts/src/main.c
@@ -20,31 +20,6 @@ void EndCritical(long sr);    // restore I bit to previous value
 void WaitForInterrupt(void);  // low power mode
 #define PF1                     (*((volatile uint32_t *)0x40025008))
 
-
-void dummy(void) { 
-	//UART_OutUDec(OS_ReadPeriodicTime()); 
-	//OutCRLF();
-}
-
-void UART_Test(void){
-  char string[20];  // global to assist in debugging
-
-  uint32_t n;
-	
-  OutCRLF();
-  UART_OutChar('-');
-  UART_OutChar('-');
-  UART_OutChar('>');
-  while(1){
-    UART_OutString("InString: ");
-    UART_InString(string,19);
-    UART_OutString(" OutString = "); UART_OutString(string); OutCRLF();
-
-    UART_OutString("InUDec: ");    n = UART_InUDec();
-    UART_OutString(" OutUDec = "); UART_OutUDec(n); OutCRLF();
-
-  }
-}
 void LCD_Init (void){
 	 ST7735_InitR(INITR_REDTAB);						// LCD Initialization
 	 ST7735_FillScreen(0x0000);							// Black screen
@@ -53,17 +28,6 @@ void LCD_Init (void){
 	 ST7735_SetCursor (0, 9);
 	 ST7735_OutString ("Device 2:");
 	 ST7735_DrawFastHLine(0, 80, 128, 0xffe0); // Horizontal line that separates the top and bottom display
-}
-
-void ADC_Test(void){
-	 // ADC Testing
-	 int32_t buffer[9];
-	 ADC_Collect (1, 1000, buffer, 9);
-	 ST7735_FillScreen (0x0000);
-	 for (int i = 0; i < 10; i++){
-		 UART_OutUDec (buffer[i]);
-		 OutCRLF();
-	 }
 }
 
 void GPIO_PortF_Init(void){   
@@ -79,6 +43,10 @@ void GPIO_PortF_Init(void){
   GPIO_PORTF_DEN_R = 0x0F;       // 7) enable digital I/O on PF0-PF3
 }
 
+void dummy(void) { 
+	PF1 ^= 0x02;
+}
+
 int main(){
 	 PLL_Init(Bus80MHz);
    ADC0_InitTimer0ATriggerSeq3(1,0);
@@ -89,45 +57,6 @@ int main(){
 	 PF1 = 0;
 	 OS_AddPeriodicThread(dummy,8000000, 1);  // 100ms
 	while(1){
-		//ADC_Test();
-		//cmdLine_Start();
-		PF1 ^= 0x02;
+		cmdLine_Start();
 	}
-}
-
-// Tests the dimensions of the two separate displays
-int main2 (){	
-	 ST7735_InitR(INITR_REDTAB);						// LCD Initialization
-	 ST7735_FillScreen(0x0000);
-	 ST7735_DrawFastHLine(0, 80, 128, 0xffe0);
-	 
-	// Top 
-	 ST7735_SetCursor (0, 0);
-	 ST7735_OutString ("Device 1");
-	 ST7735_SetCursor (0, 2);
-	 ST7735_OutString ("Line 0");
-	 ST7735_SetCursor (0, 3);
-	 ST7735_OutString ("Line 1");
-	 ST7735_SetCursor (0, 4);
-	 ST7735_OutString ("Line 2");
-	 ST7735_SetCursor (0, 5);
-	 ST7735_OutString ("Line 3");
-	 ST7735_SetCursor (0, 6);
-	 ST7735_OutString ("Line 4");
-	
-	// Bottom 
-	 ST7735_SetCursor (0, 9);
-	 ST7735_OutString ("Device 2");
-	 ST7735_SetCursor (0, 11);
-	 ST7735_OutString ("Line 0");
-	 ST7735_SetCursor (0, 12);
-	 ST7735_OutString ("Line 1");
-	 ST7735_SetCursor (0, 13);
-	 ST7735_OutString ("Line 2");
-	 ST7735_SetCursor (0, 14);
-	 ST7735_OutString ("Line 3");
-	 ST7735_SetCursor (0, 15);
-	 ST7735_OutString ("Line 4");
-	 
-	 return 0;
 }
